791a3436abf1bd63bbf9125c9e21421ac098e805
Taylor Tam <taylor@taylortam.com>
2023-05-01 09:12:40 -0700

    fixed signup page front end, flask error

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ app.secret_key = "Change Me"
 
 # TODO: Fill in methods and routes
 
-@app.before_first_request
-def setup():
-    init_db()
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    init_db()
+    app.run(port=5001)
