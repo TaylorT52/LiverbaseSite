48902d88976362beaeda8e733191a04b0c201cba
Taylor Tam <taylor@taylortam.com>
2023-05-01 10:24:25 -0700

    got sign up functionality, need to fix login

--- app.py
+++ app.py
@@ -13,13 +13,34 @@ def signin():
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
-        add_user = User(email = email, password = password)
-        db_session.add(add_user)
-        db_session.commit()
+        print(email)
+        print(password)
+        #TODO: FIX
+        user = db_session.query(User).where(User.email == email, User.password == password)
+        print(user)
+
+        if not user is None:
+            print("success!")
+        else:
+            print("this is not a valid user")
+
     return render_template("login.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        password2 = request.form["password-attempt2"]
+        
+        if password == password2:
+            add_user = User(email = email, password = password)
+            db_session.add(add_user)
+            db_session.commit()
+            print("success!")
+        else:
+            print("Doesn't match")
+
     return render_template("signup.html")
 
 if __name__ == "__main__":
