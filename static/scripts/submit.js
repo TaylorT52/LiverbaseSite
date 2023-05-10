ecc03687fd5c280c88168459f009b90a0b4596a7
Taylor Tam <taylor@taylortam.com>
2023-05-10 10:26:12 -0700

    submit slides finished

--- static/scripts/submit.js
+++ static/scripts/submit.js
@@ -1,7 +1,10 @@
+//Js all written by me
+
 const fileForm = document.getElementById("file")
 const analyzeBtn = document.getElementById("analyzeBtn")
 const popup = document.getElementsByClassName("popup")[0]
 
+//To display image after selecting
 fileForm.addEventListener("input", function(){
     var inputVal = fileForm.files[0]
 
@@ -14,6 +17,7 @@ fileForm.addEventListener("input", function(){
     }
 })
 
+//Popup while submission running
 analyzeBtn.addEventListener("click", function(){
     popup.classList.replace("hide-popup", "show-popup")
 })
